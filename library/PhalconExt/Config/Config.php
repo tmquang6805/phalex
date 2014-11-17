91bc1aeca1184812f493506286ae466ba92b6f92
QuangTM <tmquang6805@yahoo.com>
2014-11-18 01:34:58 +0700

    Init phalcon extension with mvc/applicaton and config

---
+++ library/PhalconExt/Config/Config.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: quangtm
+ * Date: 18/11/2014
+ * Time: 00:10
+ */
+
+namespace PhalconExt\Config;
+
+
+class Config
+{
+
+    private $config;
+
+    public function __construct (array $config)
+    {
+
+    }
+
+    public function getConfig ()
+    {
+        if ($this->config) {
+            return $this->config;
+        }
+
+        if (($this->config = $this->getConfigFromCache()) !== false) {
+            return $this->config;
+        }
+
+        return $this->getConfigModules()
+            ->getConfigApp()
+            ->merge()
+            ->setConfigToCache()
+            ->getCofig();
+    }
+
+}
--- library/PhalconExt/Mvc/Application.php
+++ library/PhalconExt/Mvc/Application.php
@@ -8,15 +8,33 @@
 
 namespace PhalconExt\Mvc;
 
+use PhalconExt\Config\Config;
 
 class Application
 {
-    public function __construct(array $config)
+
+    public function __construct (array $config)
     {
-        xdebug_var_dump($config);
+        $config = (new Config($config))
+            ->getConfig();
     }
 
-    public function run (){
+//    private function createAutoloadModulePaths (array $modulePaths)
+//    {
+//        $result = [];
+//        foreach ($modulePaths as $modulePath) {
+//            $modulePath = realpath($modulePath);
+//            if ($modulePath === false) {
+//                throw new \RuntimeException('Invalid module path. Check application config for module autoload paths');
+//            }
+//            array_push($result, $modulePath);
+//        }
+//
+//        return $result;
+//    }
+
+    public function run ()
+    {
         echo 'Run project';
     }
 }
