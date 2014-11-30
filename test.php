0078668b09e64c1ff7c393b817c6af9a408d5e5a
QuangTM <tmquang6805@yahoo.com>
2014-12-01 01:01:59 +0700

    delete test

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$output = array();
-$return = null;
-$fileName = 'library/Phalex/Di/DiManager.php';
-exec("php-cs-fixer fix --dry-run --level=psr2 --verbose " . escapeshellarg($fileName), $output, $return);
-xdebug_var_dump($output, $return);
