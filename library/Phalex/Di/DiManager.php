7f0638a22d01bd21adac5880fdf33fb48771795a
QuangTM <tmquang6805@yahoo.com>
2014-12-09 00:45:30 +0700

    Add create service via config (not tests)

--- library/Phalex/Di/DiManager.php
+++ library/Phalex/Di/DiManager.php
@@ -9,6 +9,7 @@
 namespace Phalex\Di;
 
 use Phalex\Mvc\Router;
+use Phalcon\Config;
 
 /**
  * Control setting/getting Phalcon DI
@@ -23,11 +24,29 @@ class DiManager
      */
     protected $diFactory;
 
+    /**
+     *
+     * @var Config
+     */
+    protected $config;
+
     public function __construct(Di $di)
     {
         $this->diFactory = $di;
     }
-    
+
+    /**
+     *
+     * @return Config
+     */
+    private function getConfig()
+    {
+        if (!$this->config instanceof Config) {
+            $this->config = $this->diFactory->get('config');
+        }
+        return $this->config;
+    }
+
     /**
      *
      * @return Di
@@ -44,7 +63,7 @@ class DiManager
      */
     public function initRouterDi()
     {
-        $config = $this->diFactory->get('config');
+        $config = $this->getConfig();
         if (!isset($config['router'])) {
             throw new Exception\RuntimeException('Cannot init DI for router. Cannot find router configuration');
         }
@@ -55,6 +74,69 @@ class DiManager
         }
 
         $this->diFactory->set('router', $router, true);
+
+        return $this;
+    }
+
+    /**
+     * Base on config service manager, invokables, create DI service
+     *
+     * @return \Phalex\Di\DiManager
+     * @throws Exception\UnexpectedValueException
+     */
+    public function initInvokableServices()
+    {
+        $smConfig = $this->getConfig()['service_manager'];
+        if (!isset($smConfig['invokables'])) {
+            return $this;
+        }
+        $invokables = $smConfig['invokables']->toArray();
+        $shared     = isset($smConfig['shared']) ? $smConfig['shared']->toArray() : [];
+        foreach ($invokables as $serviceName => $serviceClassName) {
+            if (!is_string($serviceClassName)) {
+                $msg = sprintf('Config for invokable service "%s" must be string data type', $serviceName);
+                throw new Exception\UnexpectedValueException($msg);
+            }
+            $isShare = isset($shared[$serviceName]) ? $shared[$serviceName] : true;
+            $this->diFactory->set($serviceName, $serviceClassName, $isShare);
+        }
+        return $this;
+    }
+
+    /**
+     * Base on config service manager, factories, create DI services
+     *
+     * @return \Phalex\Di\DiManager
+     * @throws Exception\UnexpectedValueException
+     * @throws Exception\RuntimeException
+     */
+    public function initFactoriedServices()
+    {
+        $smConfig = $this->getConfig()['service_manager'];
+        if (!isset($smConfig['factories'])) {
+            return $this;
+        }
+        $factories = $smConfig['factories']->toArray();
+        $shared    = isset($smConfig['shared']) ? $smConfig['shared']->toArray() : [];
+        foreach ($factories as $serviceName => $serviceConfig) {
+            if (!is_string($serviceConfig) || !($isCallable = is_callable($serviceConfig))) {
+                $msg = sprintf('Config for factories service "%s" must be string or callable', $serviceName);
+                throw new Exception\UnexpectedValueException($msg);
+            }
+
+            $isShare = isset($shared[$serviceName]) ? $shared[$serviceName] : true;
+            if ($isCallable) {
+                $this->diFactory->set($serviceName, $serviceConfig($this->diFactory), $isShare);
+                continue;
+            }
+
+            $obj = new $serviceConfig();
+            if (!$obj instanceof DiFactoryInterface) {
+                $msg = sprintf('Class "%s" must be implemented "%s"', $serviceConfig, DiFactoryInterface::class);
+                throw new Exception\RuntimeException($msg);
+            }
+            $this->diFactory->set($serviceName, $obj->createService($this->diFactory), $isShare);
+        }
 
         return $this;
     }
