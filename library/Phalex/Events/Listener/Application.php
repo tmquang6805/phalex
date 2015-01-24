c3ce6760c05d20c7589d84a37efab955a744a144
QuangTM <tmquang6805@yahoo.com>
2015-01-24 22:35:21 +0700

    Add set options when using volt template

--- library/Phalex/Events/Listener/Application.php
+++ library/Phalex/Events/Listener/Application.php
@@ -11,7 +11,9 @@ namespace Phalex\Events\Listener;
 use Phalcon\Events\Event;
 use Phalcon\Mvc\Application as PhalconApp;
 use Phalcon\Mvc\Dispatcher\Exception as DispatchException;
+use Phalcon\Config;
 use Phalex\Mvc\View;
+use Phalex\Events\Exception;
 
 /**
  * Description of Application
@@ -20,6 +22,7 @@ use Phalex\Mvc\View;
  */
 class Application
 {
+
     public function boot(Event $event, PhalconApp $app)
     {
         $router = $app->router;
@@ -30,13 +33,33 @@ class Application
         $app->getDI()->set('matchedRoute', $router->getMatchedRoute());
     }
 
+    private function setVoltOptions(Config $config, $moduleName)
+    {
+        $volt = isset($config['volt'][$moduleName]) ? $config['volt'][$moduleName]->toArray() : [];
+        if (isset($volt['path'])) {
+            $volt['path'] = realpath($volt['path']);
+            if ($volt['path'] === false) {
+                throw new Exception\RuntimeException('Not found compiled folder for volt engine');
+            }
+            $volt['path'] .= DIRECTORY_SEPARATOR;
+            if (!is_writable($volt['path'])) {
+                throw new Exception\RuntimeException('Compiled folder is not writable');
+            }
+        }
+        return $volt;
+    }
+
     public function beforeStartModule(Event $event, PhalconApp $app, $moduleName)
     {
         $di       = $app->getDI();
-        $viewsDir = $di->get('config')['view'][$moduleName];
-        $options  = [
+        $config   = $di->get('config');
+        $viewsDir = $config['view'][$moduleName];
+        $volt     = $this->setVoltOptions($config, $moduleName);
+
+        $options = [
             'di'        => $di,
             'views_dir' => $viewsDir,
+            'volt'      => $volt
         ];
         $di->set('view', new View($options), true);
     }
--- library/Phalex/Mvc/View.php
+++ library/Phalex/Mvc/View.php
@@ -19,6 +19,28 @@ use Phalex\Di\Di;
  */
 class View extends PhalconView
 {
+    /**
+     * Constructor for phalex view
+     *
+     * <ul>
+     *      <li>array['di']         <i>\Phalex\Di\Di</i>    REQUIRED</li>
+     *      <li>array['views_dir']  <i>string</i>           Sets views directory. Depending of your platform,
+     *                                                      always add a trailing slash or backslash. REQUIRED</li>
+     *      <li>array['volt']       <i>array</i>            Options when using volt template. OPTIONAL</li>
+     *      <ul>
+     *          <li>['path']            <i>string</i>           A writable path where the compiled PHP templates
+     *                                                          will be placed.</li>
+     *          <li>['extension']       <i>string</i>           An extension appended to the compiled PHP file</li>
+     *          <li>['hierarchical']    <i>bool</i>             Set compiled hierarchical. Default is FALSE</li>
+     *          <li>['separator']       <i>string</i>           Volt replaces the directory separators / and \ by
+     *                                                          this separator in order to create a single file
+     *                                                          in the compiled directory. Effect when hierarchical
+     *                                                          is FALSE</li>
+     *      </ul>
+     * </ul>
+     * @param array $options (See above)
+     * @throws Exception\InvalidArgumentException
+     */
     public function __construct(array $options)
     {
         if (!isset($options['di']) || !$options['di'] instanceof Di || !isset($options['views_dir'])) {
@@ -30,11 +52,67 @@ class View extends PhalconView
         $this->setViewsDir($options['views_dir']);
         $engines = [
             '.phtml' => Engine\Php::class,
-            '.volt'  => Engine\Volt::class,
+            '.volt'  => $this->setVoltEngine($options['di'], isset($options['volt']) ? $options['volt'] : [])
         ];
         if (isset($options['engines'])) {
             $engines = $options['engines'];
         }
         $this->registerEngines($engines);
+    }
+
+    /**
+     * Set complied path base on template path and config options
+     *
+     * @param string $templatePath template view path
+     * @param array $options config for volt engine
+     */
+    private function setCompiledPath($templatePath, $options)
+    {
+        $viewsDir = $this->getViewsDir();
+
+        $relativeViewFile = str_replace($viewsDir, '', $templatePath);
+        if (!isset($options['hierarchical']) || !$options['hierarchical']) {
+            $separator    = isset($options['separator']) ? $options['separator'] : '__';
+            $compliedPath = str_replace(DIRECTORY_SEPARATOR, $separator, $relativeViewFile);
+        } else {
+            $relativeViewPath = substr($relativeViewFile, 0, strrpos($relativeViewFile, DIRECTORY_SEPARATOR));
+            $absolutedPath    = $options['path'] . $relativeViewPath;
+            if (!is_dir($absolutedPath)) {
+                mkdir($absolutedPath, 0755, true);
+            }
+
+            if (!is_writable($absolutedPath)) {
+                throw new Exception\RuntimeException(sprintf('Cannot write compile view to "%s"', $absolutedPath));
+            }
+            $compliedPath = $relativeViewFile;
+        }
+
+        $compliedPath = $options['path'] . $compliedPath;
+        $compliedExt  = isset($options['extension']) ? $options['extension'] : '.php';
+        $compliedPath .= $compliedExt;
+
+        return $compliedPath;
+    }
+
+    /**
+     * Set volt engine with options
+     *
+     * @param Di $di
+     * @param array $options
+     * @return \Phalcon\Mvc\View\Engine\Volt
+     */
+    private function setVoltEngine(Di $di, array $options = [])
+    {
+        if (isset($options['path'])) {
+            $options['compiledPath'] = function ($templatePath) use ($options) {
+                return $this->setCompiledPath($templatePath, $options);
+            };
+        }
+
+        $engine = new Engine\Volt($this, $di);
+        if (!empty($options)) {
+            $engine->setOptions($options);
+        }
+        return $engine;
     }
 }
