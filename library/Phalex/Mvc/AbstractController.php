59a1bb7b79ec525981674bee0f4879a7797dc733
QuangTM <tmquang6805@yahoo.com>
2015-02-01 12:33:17 +0700

    Add abstract controller with support catch exception in ajax

---
+++ library/Phalex/Mvc/AbstractController.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Phalex\Mvc;
+
+use Exception;
+use Phalcon\Mvc\Controller;
+use Phalex\Http\ResponseJson;
+
+/**
+ * Description of AbstractController
+ *
+ * @author quangtm
+ */
+abstract class AbstractController extends Controller
+{
+    protected function ajaxExceptionCatch(ResponseJson &$response, Exception $exc)
+    {
+        $response->setContent(json_encode([
+            'error' => 1,
+            'data'  => [
+                'msg'   => 'Has error. Please try again later, thank you!',
+                'err'   => $exc->getMessage(),
+                'trace' => $exc->getTraceAsString(),
+            ],
+        ]));
+    }
+}
